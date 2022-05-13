982a7031aa9930495935160e6fc5b0e359eebffc
insikPark <insikk0515@gmail.com>
2022-05-14 03:30:42 +0900

    szzs

---
+++ practice2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        practice 2 실행~되니...ㄴ
+        </p>
+        a
+      </header>
+    </div>
+  );
+}
+
+export default App;
