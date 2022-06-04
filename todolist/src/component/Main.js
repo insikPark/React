1e449a6e3440c9557c3ea6b3dc0d0e737dbc2c67
insikPark <insikk0515@gmail.com>
2022-06-05 00:02:38 +0900

    간단한 todolist 앱

--- todolist/src/component/Main.js
+++ todolist/src/component/Main.js
@@ -1,12 +1,12 @@
-import './Template.css';
+import './Main.css';
 
-export default function template({children, todoLength}){   /* Template 컴포넌트 안에 TodoList 컴포넌트가 
+export default function Main({children, todoLength}){   /* Template 컴포넌트 안에 TodoList 컴포넌트가 
                 속해있기 때문에 이를 자식으로 여기고 인자로 받아와 표기 해줌*/
 
     return (
         <>        
         <div className='Header'>일정 관리</div>
-            <div className="Template">
+            <div className="Main">
                 <div className="title">오늘의 일정 ({todoLength})</div>
                 <div>{children}</div>
             </div>
