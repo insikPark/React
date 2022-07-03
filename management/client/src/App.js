43dd45b493428f4bf939a05bf500f5ad94f27f15
insikPark <insikk0515@gmail.com>
2022-07-04 05:09:08 +0900

    고객 관리 시스템

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -8,34 +8,48 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import { Paper } from '@mui/material';
+import {useEffect} from 'react';
 
 let nextId = 4;
 
 function App() {
   
-  const [customers, setCustomers] = useState([                      
-  {
-    'id' : 1,
-    'name' : '이순신',
-    'birthday' : '950515',
-    'gender' : '남',
-    'job' : '장군'
-  },
-  {
-    'id' : 2,
-    'name' : '장보고',
-    'birthday' : '970302',
-    'gender' : '남',
-    'job' : '학생'
-  }, 
-  {
-    'id' : 3,
-    'name' : '유관순',
-    'birthday' : '930212',
-    'gender' : '여',
-    'job' : '유튜버'
-  }]);
+  // const [customers, setCustomers] = useState([                      
+  // {
+  //   'id' : 1,
+  //   'name' : '이순신',
+  //   'birthday' : '950515',
+  //   'gender' : '남',
+  //   'job' : '장군'
+  // },
+  // {
+  //   'id' : 2,
+  //   'name' : '장보고',
+  //   'birthday' : '970302',
+  //   'gender' : '남',
+  //   'job' : '학생'
+  // }, 
+  // {
+  //   'id' : 3,
+  //   'name' : '유관순',
+  //   'birthday' : '930212',
+  //   'gender' : '여',
+  //   'job' : '유튜버'
+  // }]);
     
+    const [customers, setCustomers] = useState( [ ] );
+  
+    const callApi = async () => {
+      const response = await fetch('/api/customer');
+      const body = await response.json();
+      console.log(body);    //TODO: 콘솔 찍어보셈
+      return body;
+    };
+  
+    useEffect(() => {
+      callApi().then((data) => setCustomers(data));
+    }, [ ] );
+    console.log(customers);    
 
   return (
     <Paper sx={{width: '100%', overflowX : 'auto'}}>
@@ -57,6 +71,6 @@ function App() {
       </Table> 
       </Paper>      
   )
-}
 
+}
 export default App;
--- management/server.js
+++ management/server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/hello', (req, res) => {
-    res.send({message : 'Hello Express!'});
+app.get('/api/customer', (req, res) => {
+    res.send([
+        {
+            'id' : 1,
+            'name' : '이순신',
+            'birthday' : '950515',
+            'gender' : '남',
+            'job' : '장군'
+          },
+          {
+            'id' : 2,
+            'name' : '장보고',
+            'birthday' : '970302',
+            'gender' : '남',
+            'job' : '학생'
+          }, 
+          {
+            'id' : 3,
+            'name' : '유관순',
+            'birthday' : '930212',
+            'gender' : '여',
+            'job' : '유튜버'
+          }
+    ]);
 });
 
 app.listen(5000, ()=> console.log('Listening on port : 5000'));
